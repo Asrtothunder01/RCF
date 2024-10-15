f5483c448b15120ec0049c131d5c22c8b10bd637
Asrtothunder01 <olorunfemidaniel53@gmail.com>
2024-10-15 16:26:29 +0100

    New changes

--- backend/profile/admin.py
+++ backend/profile/admin.py
@@ -3,8 +3,8 @@ from .models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin (admin.ModelAdmin):
-    list_display = ['first_name',]
-    search_fields = ['first_name','user_name']
-    date_hierarchy = ['created_at']
-    list_filter = ['user_name',]
+    list_display = ('first_name',)
+    search_fields = ('first_name','user_name')
+    date_hierarchy = 'created_at'
+    list_filter = ('user_name',)
     
