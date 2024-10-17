af329a07f12a610bf732f3cd37c1ed792475bbd2
Asrtothunder01 <olorunfemidaniel53@gmail.com>
2024-10-17 19:54:25 +0100

    New changes

--- backend/payment/admin.py
+++ backend/payment/admin.py
@@ -4,9 +4,13 @@ from .models import Payment
 @admin.register(Payment)
 
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('bank_used','date_of_transaction')
-    search_fields = ('bank_used','date_of_transaction')
+    
+    list_display = ('bank_used','date_of_transaction','amount')
+    
+    search_fields = ('bank_used','date_of_transaction','amount')
+    
     date_hierarchy = 'date_of_transaction'
+    
     list_filter = ('bank_used',)
     
   
--- backend/payment/models.py
+++ backend/payment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from account.models import User
 class BaseModel(models.Model):
     id = models.UUIDField(unique = True, default = uuid.uuid4, editable = False, primary_key = True) 
     
@@ -11,11 +12,20 @@ class BaseModel(models.Model):
         
         abstract = True
 
-class Payment(BaseModel):    
+class Payment(BaseModel):
+    
+    user = models.ForeignKey(User,on_delete = models.CASCADE)    
+
     bank_used = models.CharField(max_length=100)
+    
     bank_account_name = models.CharField(max_length=100)
+    
     date_of_transaction = models.DateField()
     receipt = models.ImageField(upload_to='receipts/')
+    
+    payment_status = models.CharFields(max_length = 100, default = 'pending')
+    
+    amount = models.DecimalField(max_length = 10, decimal_places = 2)   
     
     def __str__(self):
         return f'{self.bank_account_name} - {self.bank_used}'
