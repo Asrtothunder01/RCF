39bb66f005d1e7a2dcc2559aa419060ed8af012f
Astrothunder001 <olorunfemidaniel98@gmail.com>
2025-01-09 19:56:09 -0800

    changes

--- church/sunday/models.py
+++ church/sunday/models.py
@@ -2,21 +2,33 @@ from django.db import models
 
 # Create your models here.
 class Sermon(models.Model):
-    title = models.CharField(max_length=200)
-    date = models.DateField()
-    preacher = models.CharField(max_length=100)
-    theme = models.CharField(max_length=200)
-    content = models.TextField()
-    audio_file = models.FileField(upload_to='sermons/',null=True,blank=True)
+    title = models.CharField(max_length=200, verbose_name="Sermon Title")
+    date = models.DateField(verbose_name="Date of Sermon")
+    preacher = models.CharField(max_length=100, verbose_name="Preacher's Name")
+    theme = models.CharField(max_length=200, verbose_name="Sermon Theme", null=True, blank=True)
+    content = models.TextField(verbose_name="Sermon Content")
+    audio_file = models.FileField(upload_to='sermons/', null=True, blank=True, verbose_name="Audio File")
+
+    class Meta:
+        verbose_name = "Sermon"
+        verbose_name_plural = "Sermons"
+        ordering = ['-date']  # Orders by most recent first
+
+    def __str__(self):
+        return self.title
 
 
-class school(models.Model):
-    title = models.CharField(max_length=200)
+class School(models.Model):  # Renamed to follow PascalCase convention
+    title = models.CharField(max_length=200, verbose_name="Lesson Title")
+    date = models.DateField(verbose_name="Date of Lesson")
+    topic = models.CharField(max_length=200, verbose_name="Lesson Topic")
+    memory_verse = models.CharField(max_length=300, verbose_name="Memory Verse")
+    content = models.TextField(verbose_name="Lesson Content")
 
-    date = models.DateField()
+    class Meta:
+        verbose_name = "School Lesson"
+        verbose_name_plural = "School Lessons"
+        ordering = ['-date']  # Orders by most recent first
 
-    topic = models.CharField(max_length= 200)
-
-    memory_verse = models.CharField(max_length=300)
-    content = models.TextField()
- 
+    def __str__(self):
+        return self.title
